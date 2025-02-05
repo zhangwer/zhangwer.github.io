7380447c351dce2b3f90e8274065338cab2df850
古帆 <oncwnuDNa0f8-uHClj4Du_g8O2mM@git.weixin.qq.com>
2025-02-06 04:27:02 +0800

    250206

---
+++ game/__settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2164182.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 213146958, 213146959, 213146957 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/213146976/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/213146980/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/213146971/1/ammo.js', 'preload' : true},
+];
